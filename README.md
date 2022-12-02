73ad8ec99b9116967524885549a1df808f70126a
Higanbana <vucaominh1408@gmail.com>
2022-12-02 18:13:05 +0700

    added README

---
+++ README.md
@@ -0,0 +1,6 @@
+# Advent of Code Solutions written in Rust
+To be frank, I'm not fond of competitive programming, but Advent of Code built different from my perspective.
+I used to solve AoC problems using C++ and Python, but now I want to use these problemsets to train my Rust knowledge. 
+I'm too lazy to optimize the codes, I fell into the rabbit hole just for fun, and sharpen my logical thinking.
+
+
--- aoc2022/day02/src/main.rs
+++ aoc2022/day02/src/main.rs
@@ -1,12 +1,12 @@
 use std::{fs::File, io, io::BufRead, path::Path, hash::Hash, collections::HashMap};
 
-fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>//{{{
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
 where
     P: AsRef<Path>,
 {
     let file = File::open(filename)?;
     Ok(io::BufReader::new(file).lines())
-}//}}}
+}
 
 fn main() {
     let mut score1 = 0;
@@ -17,7 +17,7 @@ fn main() {
     bonus_map.insert('B' as i32, 2); // PAPER
     bonus_map.insert('C' as i32, 3); // SCISSOR
                                      //
-    if let Ok(lines) = read_lines("input1.txt") {{{{    
+    if let Ok(lines) = read_lines("input1.txt") {
         for line in lines {
             if let Ok(ip) = line {
                 let mut split = ip.split(' ');
@@ -35,7 +35,7 @@ fn main() {
                 score1 += *bonus_map.get(&player_ascii).unwrap();
             }
         }
-    }}}}
+    }
 
     if let Ok(lines) = read_lines("input2.txt") {
         for line in lines {
